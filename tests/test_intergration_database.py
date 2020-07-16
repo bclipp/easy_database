1f65690c084052189f72a10b5564dab4defa8ad1
blipp <bclipp770@gmail.com>
2020-07-16 06:09:46 -0400

    more integration tests

--- tests/test_intergration_database.py
+++ tests/test_intergration_database.py
@@ -1,25 +1,27 @@
-from unittest.mock import Mock
-from unittest.mock import patch
+"""
+This module is used for integration tests to verify functionality with the postgresql database
 
-import pandas as pd
+    export POSTGRES_DB=testing1234
+    export POSTGRES_USER=testing1234
+    export POSTGRES_PASSWORD=testing1234
+    export DB_IP_ADDRESS=127.0.0.1
+
+"""
 
 import easydb.database as db
 
 
 def test_connect_disconnect():
     """
-    This test is to verify the send_sql method works functionally without a easydb.
-    export POSTGRES_DB=testing1234
-    export POSTGRES_USER=testing1234
-    export POSTGRES_PASSWORD=testing1234
-    export DB_IP_ADDRESS=127.0.0.1
+    test_connect_disconnect to verify the send_sql method works functionally without a easydb.
     """
-    username = "projectetl"
-    password = "projectetl"
-    hostname = "127.0.0.1"
-    database = "projectetl"
-    # table = "testing1234"
-    connection_string: str = f"""postgresql://{hostname}/{database}?user={username}&password={password}"""
+    dbvars = set_db_variables()
+    hostname = dbvars["hostname"]
+    database = dbvars["database"]
+    username = dbvars["username"]
+    password = dbvars["password"]
+    connection_string: str = \
+        f"postgresql://{hostname}/{database}?user={username}&password={password}"
     database_manager = db.DatabaseManager(connection_string)
     database_manager.connect_db()
     database_manager.close_conn()
@@ -27,20 +29,42 @@ def test_connect_disconnect():
 
 def test_receive_sql_fetchall():
     """
-    This test is to verify the send_sql method works functionally without a easydb.
-    export POSTGRES_DB=testing1234
-    export POSTGRES_USER=testing1234
-    export POSTGRES_PASSWORD=testing1234
-    export DB_IP_ADDRESS=127.0.0.1
+    test_receive_sql_fetchall is to verify the send_sql method works functionally without a easydb.
     """
-    username = "projectetl"
-    password = "projectetl"
-    hostname = "127.0.0.1"
-    database = "projectetl"
-    # table = "testing1234"
-    connection_string: str = f"""postgresql://{hostname}/{database}?user={username}&password={password}"""
+    dbvars = set_db_variables()
+    hostname = dbvars["hostname"]
+    database = dbvars["database"]
+    username = dbvars["username"]
+    password = dbvars["password"]
+    connection_string: str = \
+        f"postgresql://{hostname}/{database}?user={username}&password={password}"
     database_manager = db.DatabaseManager(connection_string)
     database_manager.connect_db()
-    data_frame:pd.DataFrame = database_manager.receive_sql_fetchall("SELECT * FROM test;")
+    data_frame: list = database_manager.receive_sql_fetchall(f"SELECT * FROM {table};")
     print(data_frame)
     database_manager.close_conn()
+
+
+def test_send_sql():
+    """
+    test_send_sql is to verify the send_sql method works functionally without a easydb.
+    """
+    dbvars = set_db_variables()
+    hostname = dbvars["hostname"]
+    database = dbvars["database"]
+    username = dbvars["username"]
+    password = dbvars["password"]    connection_string: str = \
+        f"postgresql://{hostname}/{database}?user={username}&password={password}"
+    database_manager = db.DatabaseManager(connection_string)
+    database_manager.connect_db()
+    database_manager.send_sql(f"SELECT * FROM {table};")
+    database_manager.close_conn()
+
+
+def set_db_variables():
+    dbvars = {"username": "testing1234",
+              "password": "testing1234",
+              "hostname": "127.0.0.1",
+              "database": "testing1234",
+              "table": "customers"}
+    return dbvars
