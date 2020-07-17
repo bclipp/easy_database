5d89da31b0c5bd2735fea0cb324dc19b4cace9d2
blipp <bclipp770@gmail.com>
2020-07-17 07:44:41 -0400

    fixing ci workflow

--- easydb/utils.py
+++ easydb/utils.py
@@ -23,19 +23,11 @@ def get_variables() -> ConfigVars:
     get_variables is used to access environmental variables
     :return:
     """
-    try:
-        db_ip_address = os.environ['DB_IP_ADDRESS']
-        postgres_db = os.environ['POSTGRES_DB']
-        postgres_user = os.environ['POSTGRES_USER']
-        postgres_password = os.environ['POSTGRES_PASSWORD']
+    try
         integration_test = os.environ.get('INTEGRATION_TEST', default=None)
     except KeyError:
         raise KeyError("Please verify that the needed env variables are set")
-    return {"db_ip_address": db_ip_address,
-            "postgres_db": postgres_db,
-            "postgres_user": postgres_user,
-            "postgres_password": postgres_password,
-            "integration_test": integration_test}
+    return {"integration_test": integration_test}
 
 
 def check_integration_test():
