21cf6d1d35d966ac454550b4b08c696a744bb089
blipp <bclipp770@gmail.com>
2020-07-20 22:28:36 -0400

    fixing unit tests in ci

--- easy_database/utils.py
+++ easy_database/utils.py
@@ -19,7 +19,7 @@ class ConfigVars(TypedDict):
     password: str
     table: Optional[str]
     integration_test: Optional[str]
-    database_type: str
+    database_type: Optional[str]
 
 
 def get_variables() -> ConfigVars:
@@ -34,7 +34,7 @@ def get_variables() -> ConfigVars:
         password: str = os.environ['PASSWORD']
         table: Optional[str] = os.environ.get('TABLE', default=None)
         integration_test: Optional[str] = os.environ.get('INTEGRATION_TEST', default=None)
-        database_type: str = os.environ["DATABASE_TYPE"]
+        database_type: str = os.environ.get('DATABASE_TYPE', default=None)
     except KeyError:
         raise KeyError("Please verify that the needed env variables are set")
     return {"db_ip_address": db_ip_address,
